e6f1c6e7a26381e78be9eeb5add7060c14ee1deb
shas <noahmansland@gmail.com>
2020-09-10 08:40:56 +0200

    Pushed the project

---
+++ EpitechJam2020Feb-master/Assets/Scripts/Controller/SpaceShipController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class SpaceShipController : MonoBehaviour
+{
+    private Vector2 walkingVector;
+    private Vector2 Look;
+    [SerializeField] [Header("Speed Factor")] private float speed = 20;
+    [SerializeField] [Header("Camera, body")] private GameObject Camera = default(GameObject);
+
+    [SerializeField] [Header("Mouse X sensibility")] private float XSensibility = 1f;
+    [SerializeField] [Header("Mouse Y sensibility")] private float Ysensibility = 1f;
+    private Rigidbody RigidbodyCorps = null;
+    private float CameraYOffSet;
+
+    void Start()
+    {
+        if (!RigidbodyCorps) RigidbodyCorps = gameObject.GetComponent<Rigidbody>();
+        if (!RigidbodyCorps) RigidbodyCorps = gameObject.AddComponent<Rigidbody>();
+        CameraYOffSet = Camera.transform.localPosition.y;
+        RigidbodyCorps.useGravity = false;
+    }
+    public void Update()
+    {
+		var velocityChange = (((walkingVector.y * transform.forward) * speed * Time.deltaTime));
+        RigidbodyCorps.AddForce(velocityChange, ForceMode.VelocityChange);
+        Rotate();
+    }
+
+    public void Rotate()
+    {
+         var rotation = transform.rotation.eulerAngles;
+        RigidbodyCorps.AddTorque((transform.right * - Look.y * Ysensibility + transform.up * Look.x * XSensibility + walkingVector.x * transform.forward * speed)* Time.deltaTime);
+    }
+
+    public void OnLook(InputValue value)
+    {
+        Look = value.Get<Vector2>();
+    }
+
+    public void OnWalk(InputValue value)
+    {
+        walkingVector = value.Get<Vector2>();
+    }
+
+    public void OnBreak()
+    {
+        RigidbodyCorps.velocity  /= 2;
+        RigidbodyCorps.angularVelocity /= 2;
+        
+    }
+}
---
+++ EpitechJam2020Feb-master/Assets/Scripts/Gravity/FauxGravityBody.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravityBody : MonoBehaviour
+{
+    public int radius;
+    public LayerMask mask;
+    private FauxGravityAttractor attractor;
+    private Transform myTransform;
+    private CharacterController charCtrl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTransform = gameObject.GetComponent<Transform>();
+    }
+
+    GameObject getClosestPlanet()
+    {
+        GameObject closest_planet = null;
+        Vector3 origin = transform.position;
+        float min_dist = float.MaxValue;
+        float hit_dist = 0;
+
+        var hits = Physics.SphereCastAll(origin, radius, transform.forward);
+        if (hits.Length > 0)
+        {
+            foreach(var hit in hits) {
+                if (hit.transform.gameObject.layer != 8)
+                    continue;
+                hit_dist = Vector3.Distance(transform.position, hit.transform.position);
+                if (min_dist > hit_dist) {
+                    min_dist = hit_dist;
+                    closest_planet = hit.transform.gameObject;
+                }
+            }
+            return closest_planet;
+        }
+        return(null);
+    }
+
+    void Update()
+    {
+        var closest_planet = getClosestPlanet();
+        FauxGravityAttractor closest_planet_attractor;
+        if (closest_planet != null)
+            closest_planet_attractor = closest_planet.GetComponent<FauxGravityAttractor>();
+        else {
+            attractor = null;
+            return;
+        }
+        if (attractor != closest_planet_attractor)
+            attractor = closest_planet_attractor;
+        if (attractor != null)
+            attractor.Attract(myTransform);  
+    }
+}
